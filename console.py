406f9e0d1f5c6534b6b9a2a1c02b7bc77fc085a5
me <nmapsy@gmail.com>
2015-05-21 17:36:19 -0400

    console.py: Add Windows method.

--- console.py
+++ console.py
@@ -1,5 +1,5 @@
 import os
-__version__ = '0.1'
+__version__ = '1.0'
 
 DEFAULT_WIDTH = 70
 DEFAULT_HEIGHT = 24
@@ -24,11 +24,18 @@ def termsize(default_height=None, default_width=None):
   defaults:         Defaults given by named arguments to termsize().
   DEFAULTs:         Module defaults (DEFAULT_WIDTH, DEFAULT_HEIGHT).
   """
+  # Use Unix methods by default, unless the platform is definitely Windows.
+  import platform
+  family = platform.system()
+  if family.lower() == 'windows':
+    methods = (termsize_win, termsize_stty, termsize_env)
+  else:
+    methods = (termsize_ioctl, termsize_stty, termsize_env)
   if default_height is None:
     default_height = DEFAULT_HEIGHT
   if default_width is None:
     default_width = DEFAULT_WIDTH
-  for method in (termsize_ioctl, termsize_stty, termsize_env):
+  for method in methods:
     (height, width) = method()
     if height is not None and width is not None:
       return height, width
@@ -101,3 +108,26 @@ def termsize_env():
     except ValueError:
       height = None
   return (height, width)
+
+
+# from: https://code.activestate.com/recipes/440694-determine-size-of-console-window-on-windows/
+def termsize_win():
+  import ctypes
+  import struct
+
+  try:
+    h = ctypes.windll.kernel32.GetStdHandle(-12)
+    csbi = ctypes.create_string_buffer(22)
+    res = ctypes.windll.kernel32.GetConsoleScreenBufferInfo(h, csbi)
+  except AttributeError:
+    return (None, None)
+  if not res:
+    return (None, None)
+
+  (a, b, c, d, e, left, top, right, bottom, j, k) = struct.unpack("hhhhHhhhhhh", csbi.raw)
+  try:
+    height = bottom - top + 1
+    width = right - left + 1
+    return (height, width)
+  except TypeError:
+    return (None, None)
