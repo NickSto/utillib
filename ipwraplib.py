9fafead3535346324a39c9404a59ba9ce47b6e78
me <nmapsy@gmail.com>
2016-03-23 01:11:32 -0700

    ipwraplib.py: Add local_to_global_mac(), fix get_random_mac(). - get_random_mac() was generating local MAC addresses instead of global ones.

--- ipwraplib.py
+++ ipwraplib.py
@@ -109,6 +109,7 @@ def dig_ip(domain):
 def dns_query(domain):
   """Use the socket module to do a DNS query.
   Returns None on failure instead of raising an exception (like socket.gaierror)."""
+  #TODO: Looks like getaddrinfo() is the preferred way?
   try:
     return socket.gethostbyname(domain)
   except socket.error:
@@ -159,17 +160,38 @@ def get_mac():
 
 def get_random_mac():
   """Generate a valid, random MAC address."""
-  octets = []
   # In the first byte, the two least-significant bits must be 10:
   # The 1 means it's a local MAC address (not globally assigned and unique).
   # The 0 means it's not a broadcast address.
   # https://superuser.com/questions/725467/set-mac-address-fails-rtnetlink-answers-cannot-assign-requested-address/725472#725472
-  octet = random.randint(0, 63)*4 + 2
-  octets.append('{:02x}'.format(octet))
+  octet1_int = random.randint(0, 63)*4
+  octets = ['{:02x}'.format(octet1_int)]
   for i in range(5):
-    octet = random.randint(0, 255)
-    octets.append('{:02x}'.format(octet))
+    octet_int = random.randint(0, 255)
+    octets.append('{:02x}'.format(octet_int))
   return ':'.join(octets)
+
+
+def local_to_global_mac(mac_input):
+  """Alter a MAC address by setting its "locally administered" bit to 0.
+  If there are alphabetic characters in the MAC, the output will match their case."""
+  octets = mac_input.split(':')
+  octet1_int_input = int(octets[0], 16)
+  # Check if the second bit is 1.
+  if octet1_int_input & 2:
+    # If so, flip it with an XOR.
+    octet1_int_global = octet1_int_input ^ 2
+  else:
+    # Otherwise, it's a global MAC already. Return unchanged.
+    return mac_input
+  octet1_global = '{:02x}'.format(octet1_int_global)
+  octets[0] = octet1_global
+  mac_global = ':'.join(octets)
+  # Follow capitalization of the input.
+  for alpha in ('A', 'B', 'C', 'D', 'E', 'F'):
+    if alpha in mac_input:
+      return mac_global.upper()
+  return mac_global
 
 
 def get_ip():
